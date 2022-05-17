ff334f65a848fe8db37bfebff1c2a05425d4ecc1
Mia <miacheng913.com>
2022-05-17 20:41:21 +0800

    5/17

--- main.py
+++ main.py
@@ -15,11 +15,19 @@ def draw() :
     x = np.linspace(0.0, 2*np.pi)  # 50x1 array between 0 and 2*pi
     y = np.cos(x)                  # cos(x)
 
-    plt.plot(x, y, 'r')   # red line without marker
-    plt.show()
+    x3 = np.linspace(0, 2*np.pi,10) # 10x1 array
+    y3 = np.sinc(x3)
+
+    plt.clf()
+    plt.grid(True)
+    plt.plot(x3, y3, 'r:')     # red dotted line (no marker)
+    plt.savefig('static/img/fig1.png')
+    
+    plt.clf()
+    plt.grid(True)
     plt.plot(x, y, 'b-o') # blue solid line with filled circle marker
-    plt.savefig('fig1.png')
-    plt.show()
+    plt.savefig('static/img/fig2.png')
+    
 
 
 templates = Jinja2Templates(directory='templates')
@@ -45,4 +53,5 @@ async def index(request: Request):
 @app.get('/home', response_class=HTMLResponse)
 async def index(request: Request):
     print(request)
+    draw()
     return templates.TemplateResponse(name='home.html', context={'request': request})
